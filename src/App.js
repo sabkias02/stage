de487a94769678784d032dd25d8860a1e047db45
Sabrina <sabrina@MacBook-Pro-de-Sabrina.local>
2022-10-31 16:23:52 -0400

    first group cards

--- src/App.js
+++ src/App.js
@@ -1,44 +1,51 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import CardGroup from "react-bootstrap/CardGroup";
 
-import React from 'react';
-
-import Navbar from './Headers/Navbar';
-import './App.css';
-
+import Navbar from "./Headers/Navbar";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App" >
-      
-       
-      <Navbar/>
-      <main id="main-content">
-        <div class="slider-one rn-section-gapTop">
-      <div class="container">
-        <div class="row row-reverce-sm align-items-center"></div>
-      <div class="col-lg-5 col-md-6 col-sm-12 mt_sm--50">
-<h1 class="title sal-animate" data-sal-delay="200" 
-data-sal="slide-up" 
-data-sal-duration="800">
-  Discover Digital Pieces Of Black History In The Metaverse</h1>
+    <div className="App">
+      <Navbar />
+      <CardGroup>
+        <Card>
+          <Card.Body className="card1">
+            <Card.Text style={{ fontSize: "80px" }}>
+              Discover Digital
+              <br /> Pieces Of Black
+              <br /> History In The Metaverse
+            </Card.Text>
+            <div className="button-group">
+              <Button
+                href="/"
+                className="btn btn-large btn-primary sal-animate btn-space"
+                data-sal-delay="400"
+                data-sal="slide-up"
+                data-sal-duration="800"
+              >
+                Become a verifier
+              </Button>
 
-
-    
-    
-    <p class="slide-disc sal-animate" data-sal-delay="300" data-sal="slide-up" data-sal-duration="800"></p>
-<div class="button-group">
-<a href="/" class="btn btn-large btn-primary sal-animate" data-sal-delay="400" data-sal="slide-up" data-sal-duration="800">
-<span>Become a verifier</span>
-</a>
-<a href="/create" class="btn btn-large btn-secondary sal-animate" data-sal-delay="500" data-sal="slide-up" data-sal-duration="800">
-<span>Create Proposal</span>
-</a>
-</div>
-</div>
-</div>
-</div>
-</main>
+              <Button
+                href="/create"
+                className="btn btn-large btn-secondary sal-animate btn-space"
+                data-sal-delay="500"
+                data-sal="slide-up"
+                data-sal-duration="800"
+              >
+                Create Proposal
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img src={require("./slider-1.jpeg")} />
+        </Card>
+      </CardGroup>
     </div>
-    
   );
 }
 
