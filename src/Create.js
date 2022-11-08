4b1fee9bdd0617fdfda569ab4cc9225ca799d757
Sabrina <sabrina@MacBook-Pro-de-Sabrina.local>
2022-11-07 19:39:42 -0500

    css de create

--- src/Create.js
+++ src/Create.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./Create.css";
 import "mdbreact/dist/css/mdb.css";
 import { MDBFile } from "mdb-react-ui-kit";
-import { Button } from "@mui/material";
-
 import Box from "@mui/material/Box";
 
 const Create = () => {
@@ -14,13 +12,20 @@ const Create = () => {
         style={{ backgroundColor: "rgb(46, 43, 43)" }}
       >
         <div class="row">
-          <div class="col-lg-6" fullWidth style={{ color: "red" }}>
-            <MDBFile
-              label="Choose a Cover/NFT
+          <div class="col-lg-6" fullWidth style={{}}>
+            <div
+              className="upload-area"
+              style={{ border: "dashed", color: "red" }}
+            >
+              <div clasName="upload">
+                <MDBFile
+                  label="Choose a Cover/NFT
                     
                       PNG, GIF, WEBP, MP4 or MP3. Max 1Gb."
-              id="customFile"
-            />
+                  id="customFile"
+                />
+              </div>
+            </div>
           </div>
           <div
             className="col-lg-6"
@@ -54,53 +59,35 @@ const Create = () => {
                 placeholder="e.g. “Martin Luther King Jr. was a social activist and Baptist minister who played a key role in the American civil rights movement from the mid-1950s until his assassination in 1968.”"
                 name="discription"
               ></textarea>
-              <div className="col-md-12 col-xl-8 mt_lg--15 mt_md--15 mt_sm--15">
-                <div className="input-box">
-                  <button
-                    className="btn btn-large btn-primary-alta w-100 d-block"
-                    type="submit"
-                    data-btn="preview"
-                  >
-                    <span>Preview</span>
-                  </button>
-                </div>
-              </div>
-              <div className="col-md-12 col-xl-8 mt_lg--15 mt_md--15 mt_sm--15">
-                <div className="input-box">
-                  <button
-                    className="btn btn-large btn-primary w-100 d-block"
-                    type="submit"
-                  >
-                    <span>Make Proposal</span>
-                  </button>
-                </div>
+              <div className="btn-group">
+                <button
+                  className="btn btn-large btn-secondary btn-space "
+                  type="submit"
+                  data-btn="preview"
+                  style={{
+                    margin: "5px 15px 5px 35px ",
+                    borderRadius: "10px",
+                    maxHeight: " max-content",
+                  }}
+                >
+                  <span>Preview</span>
+                </button>{" "}
+                <button
+                  className="btn btn-large btn-primary btn-space  "
+                  type="submit"
+                  style={{
+                    margin: "5px 35px 5px 15px ",
+                    borderRadius: "10px",
+                    maxHeight: " max-content",
+                  }}
+                >
+                  <span>Make Proposal</span>
+                </button>
               </div>
             </Box>
           </div>
         </div>
       </div>
-
-      {/*<div style={{ color: "black" }} className="create-area rn-section-gapTop">
-        <form action="#">
-          
-               
-                        >
-                         
-              <div className="mt--100 mt_sm--30 mt_md--30 d-block d-lg-none">
-                <h5> Note: </h5>
-                <span>
-                  All Products are subject to<strong>community approval</strong>{" "}
-                </span>{" "}
-                <br />
-                <span>
-                  {" "}
-                  You will receive :<strong>BLACK HISTORY DAO</strong>
-                </span>
-              </div>
-            </div>
-          </div>
-        </form>
-      </div>*/}
     </div>
   );
 };
