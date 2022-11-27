f0cc91c83da4d6fe72f2f1119daba82e3ef723fc
Sabrina <sabrina@MacBook-Pro-de-Sabrina.local>
2022-11-27 14:43:16 -0500

    circularProgressBar

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,30 +1,79 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import "./ProgressBar.scss";
 import "react-toastify/dist/ReactToastify.css";
-import * as Icon from "react-feather";
+import { gsap } from "gsap-trial/dist/gsap";
+import { DrawSVGPlugin } from "gsap-trial/dist/DrawSVGPlugin";
+import { ScrollToPlugin } from "gsap-trial/dist/ScrollToPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const ProgressBar = () => {
-  const [scrollTop, setScrollTop] = useState(0);
-  const onScroll = () => {
-    const winScroll = document.documentElement.scrollTop;
-    const height =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
-    const scrolled = (winScroll / height) * 100;
-    setScrollTop(scrolled);
-  };
+  const ref = useRef(null);
 
   useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
+    gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin);
+    const element = ref.current;
+    gsap.fromTo(
+      element.querySelector(".progress-wrap path"),
+      {
+        drawSVG: "0",
+      },
+      {
+        drawSVG: "100%",
+        scrollTrigger: {
+          trigger: element.querySelector(".progress-wrap path"),
+          start: 0,
+          end: "max",
+
+          scrub: true,
+        },
+      }
+    );
+  }, []);
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin);
+    const element = ref.current;
+    // show/hide
+    gsap.fromTo(
+      element.querySelector(".progress-wrap"),
+      {
+        yPercent: 100,
+        autoAlpha: 0,
+      },
+      {
+        yPercent: 0,
+        autoAlpha: 1,
+        scrollTrigger: {
+          trigger: element.querySelector(".progress-wrap"),
+          start: 5,
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
   }, []);
 
+  // on click, scroll back to top
+  useEffect(() => {
+    gsap.registerPlugin(ScrollToPlugin);
+    document.querySelector(".progress-wrap").addEventListener("click", (e) => {
+      gsap.to(window, {
+        scrollTo: 0,
+        duration: 0.55,
+      });
+    });
+  }, []);
   return (
     <div>
-      <div id="progress">
-        <span id="progress-value" style={{ width: "${scrollTop}%" }}>
-          &#x2B61;
-        </span>
+      <div ref={ref} className="paginacontainer">
+        <div className="progress-wrap active-progress">
+          <svg
+            className="progress-circle svg-content"
+            width="100%"
+            height="100%"
+            viewBox="-1 -1 102 102"
+          >
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+          </svg>
+        </div>
       </div>
     </div>
   );
